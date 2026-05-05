432dabcfc354bd1687996de75b94a27272270a5d
fareltek <fareltek@gmail.com>
2026-05-05 13:20:14 +0300

    Add SSE subscriber/event logging for debugging

--- src/main/java/com/fareltek/fsignal/api/MonitorController.java
+++ src/main/java/com/fareltek/fsignal/api/MonitorController.java
@@ -2,6 +2,8 @@ package com.fareltek.fsignal.api;
 
 import com.fareltek.fsignal.tcp.TcpConnectionHandler;
 import com.fareltek.fsignal.tcp.TcpDataEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +15,8 @@ import java.util.Map;
 
 @RestController
 public class MonitorController {
+
+    private static final Logger log = LoggerFactory.getLogger(MonitorController.class);
 
     private final TcpConnectionHandler handler;
 
@@ -34,6 +38,10 @@ public class MonitorController {
         Flux<TcpDataEvent> heartbeat = Flux.interval(Duration.ofSeconds(15))
                 .map(i -> TcpDataEvent.heartbeat());
 
-        return Flux.merge(handler.getDataStream(), heartbeat);
+        return Flux.merge(handler.getDataStream(), heartbeat)
+                .doOnSubscribe(s -> log.info("[SSE] Browser baglandi"))
+                .doOnNext(e -> log.info("[SSE] Event gonderiliyor: {} bytes", e.byteCount()))
+                .doOnCancel(() -> log.info("[SSE] Browser baglantisi kesildi"))
+                .doOnError(e -> log.error("[SSE] Hata: {}", e.getMessage()));
     }
 }
